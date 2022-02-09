66655c7b37392b5b10c5c1322c4a4e63ea6a9cfd
Octtavo <aotavio061@gmail.com>
2022-02-09 12:47:56 -0300

    Initial commit

---
+++ Comentarios/script.js
@@ -0,0 +1,4 @@
+// Está é uma forma de adicionar comentarios ao nosso codigo, assim podemos informar o que iremos adicionar ao nossso código, essa é a primeira forma de adicionar comentarios
+
+
+/* Essa é a segunda forma de adicionar comentarios, (comentarios de multiplas linhas). Ao contrario da forma acima, usando esse tipo de comentario você pode adicionar comentarios em varias linha, já o tipo acima só permite adicionar comentarios na propria linha, ou seja, se pularmos alguma linha ela para de funcionar, e tudo que digitarmos passa a ser código */
