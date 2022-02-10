546af719fe8cfbb4f43a60466d8eb9014d42a94f
Octtavo <aotavio061@gmail.com>
2022-02-10 15:07:02 -0300

    Usando uma function

---
+++ Criando funções/script.js
@@ -0,0 +1,12 @@
+function trocarDiv(nome) {
+    var area = document.getElementById("area");
+    var texto = prompt("Qual seu sobrenome?");
+
+    area.innerHTML = nome+" "+texto  
+}
+function fazerAcao(certo) {
+    var titulo = document.getElementById("titulo");
+    var mudar = prompt("Mude");
+
+    titulo.innerHTML = certo+" "+mudar
+}
