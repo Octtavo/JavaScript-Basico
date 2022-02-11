7fde2b78a04dc00e914bf4855984aa9754aaa0e5
Octtavo <aotavio061@gmail.com>
2022-02-11 10:12:15 -0300

    Aulas novas do curso basico de JS

---
+++ Modificando lista/script.js
@@ -0,0 +1,9 @@
+function adicionaringrediente() {
+    var ing = document.getElementById("ingrediente").value;
+
+    var listahtml = document.getElementById("lista").innerHTML;
+
+    listahtml = listahtml + "<li>"+ing+"</li>";
+    
+    document.getElementById("lista").innerHTML = listahtml;
+}
---
+++ arrays/script.js
@@ -0,0 +1,5 @@
+/*
+estrutura de uma arrays básica, para testar use o devtools do google
+*/
+
+var lista = ["arroz","feijao","caner","suco"]
