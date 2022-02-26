0e177a9126ce9c0fe78a840950505b330bce1bba
Octtavo <aotavio061@gmail.com>
2022-02-26 12:55:31 -0300

    ...

---
+++ Loops/script.js
@@ -0,0 +1,12 @@
+/*var x = 0;
+ document.write("Iniciando o Loop... <br/>")
+while(x < 10) {
+    document.write("Numero: "+x+" <br/>")
+    x++;
+}
+document.write("Finalizando o Loop...<br/>")
+*/
+
+for(x = 0; x < 10; $x++ ) {
+      document.write("Numero: "+x+" <br/>")
+}
--- Manipulacao de arrays/script.js
+++ Manipulacao de arrays/script.js
@@ -1,1 +1,13 @@
 var lista = ["arroz", "feijão", "macarrão", "carne", 20]
+
+//exemplos de manipulação 
+
+//lista.indexOf("feijão") Buscar um valor no array
+
+//lista.join(",") Para transformar array em string com todos os valores do array e a "," serve para pontuações normais do pt, podemos substituir por barra, ponto etc.
+
+//lista.pop() Para remover o ultimo item da lista de array
+
+//lista.shift() Para remover o primeiro item da lista de array
+
+//lista.push("teste") Para adicionar um novo valor a arrays
