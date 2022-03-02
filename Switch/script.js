c3ed4f6daa3dc5f32178c3fbcceb804d98de419e
Octtavo <aotavio061@gmail.com>
2022-03-02 10:16:14 -0300

    fim do js basico

---
+++ Switch/script.js
@@ -0,0 +1,16 @@
+x = prompt("Qual é o número")
+
+switch(x) {
+    case "0":
+        alert("X é o 0")
+        break
+    case "1":
+        alert("X é 1")
+        break
+    case "2":
+        alert("X vai ser 2")
+        break
+    default:
+        alert("Ningué foi satisfeito")
+        break
+}
---
+++ advinhe o numero/script.js
@@ -0,0 +1,16 @@
+function verificar() {
+    var n1 = document.getElementById("n1").innerHTML;
+    var n2 = document.getElementById("n2").value;
+
+    if(n1 == n2){
+        alert("VOCÊ ACERTOU O NÚMERO")
+    } else{
+        alert("VOCÊ ERROU!, TENTE NOVAMENTE")
+    }
+    resetar()
+}
+function resetar() {
+    document.getElementById("n2").value = ""
+    var r = Math.floor(Math.random() * 100)
+    document.getElementById("n1").innerHTML = r;
+}
